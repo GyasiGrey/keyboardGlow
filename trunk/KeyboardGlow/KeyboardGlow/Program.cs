3be1de0f2e969733fa20c3342bebe991c070f8d9
gyasi <gyasi@b7a37dab-9947-4804-b8bf-0e90ae98746a>
2016-12-15 04:19:34 +0000

    Initial commit

---
+++ trunk/KeyboardGlow/KeyboardGlow/Program.cs
@@ -0,0 +1,48 @@
+﻿using CUE.NET;
+using CUE.NET.Devices.Keyboard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyboardGlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CueSDK.Initialize();
+            CueSDK.UpdateMode = CUE.NET.Devices.Generic.Enums.UpdateMode.Continuous;
+            CueSDK.UpdateFrequency = 1f / 30f;
+
+            CorsairKeyboard keyboard = CueSDK.KeyboardSDK;
+
+            bool goingUp = true;
+            byte green = 0;
+
+            while(true)
+            {
+                if(goingUp)
+                {
+                    green++;
+                    if(green >= 255)
+                    {
+                        goingUp = false;
+                    }
+                }
+                else
+                {
+                    green--;
+                    if(green <= 0)
+                    {
+                        goingUp = true;
+                    }
+                }
+
+                keyboard['Q'].Color = new CUE.NET.Devices.Generic.CorsairColor(0, green, 0);
+                System.Threading.Thread.Sleep(1);
+            }
+        }
+    }
+}
